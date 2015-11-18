ded804a91c09cd8f7ace324ad5abf3771e6dfad4
Ignacio Giss <nacho@linux.pc>
2015-11-18 00:56:42 -0300

    random forest ej

--- SF_Crime/SF_Crime.cpp
+++ SF_Crime/SF_Crime.cpp
@@ -11,9 +11,41 @@
 #include "CSV_reader.h"
 #include <shark/Data/Csv.h> 
 #include <shark/Algorithms/Trainers/RFTrainer.h>
-#include <shark/ObjectiveFunctions/Loss/ZeroOneLoss.h> 
+#include <shark/ObjectiveFunctions/Loss/ZeroOneLoss.h>
+#include <iostream>  
+
+using namespace std; 
+using namespace shark;
 
 int main(void) {
-	puts("!!!Hello World!!!");
+
+	ClassificationDataset data;
+	importCSV(data, "data/C.csv", LAST_COLUMN, ' ');
+	   
+	//Split the dataset into a training and a test dataset
+	ClassificationDataset dataTest = splitAtElement(data,311);
+
+	cout << "Training set - number of data points: " << data.numberOfElements()
+		<< " number of classes: " << numberOfClasses(data)
+		<< " input dimension: " << inputDimension(data) << endl;
+ 
+	cout << "Test set - number of data points: " << dataTest.numberOfElements()
+		<< " number of classes: " << numberOfClasses(dataTest)
+		<< " input dimension: " << inputDimension(dataTest) << endl;
+    
+	//Generate a random forest
+	RFTrainer trainer;
+	RFClassifier model;
+	trainer.train(model, data);
+
+	// evaluate Random Forest classifier
+	ZeroOneLoss<unsigned int, RealVector> loss;
+	Data<RealVector> prediction = model(data.inputs());
+	cout << "Random Forest on training set accuracy: " << 1. - loss.eval(data.labels(), prediction) << endl;
+	
+	prediction = model(dataTest.inputs());
+	cout << "Random Forest on test set accuracy:     " << 1. - loss.eval(dataTest.labels(), prediction) << endl;
+
+
 	return EXIT_SUCCESS;
 }
